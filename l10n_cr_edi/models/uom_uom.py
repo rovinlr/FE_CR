4b2a88994fcbb834ab929d74998707bbec14900d
rovinlr <rovinlr@gmail.com>
2025-10-02 00:53:20 -0600

    Adjust UoM CR code placement

--- l10n_cr_edi/models/uom_uom.py
+++ l10n_cr_edi/models/uom_uom.py
@@ -5,6 +5,6 @@ class UomUom(models.Model):
     _inherit = "uom.uom"
 
     l10n_cr_code = fields.Char(
-        string="Código unidad CR",
+        string="Código EDI (CR)",
         help="Código de unidad de medida según el catálogo oficial de Hacienda.",
     )
